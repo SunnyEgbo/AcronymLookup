b506bd931bea792f35233a37592fb570a11dd5a2
sunnyegbo <sunnyegbo@yahoo.com>
2017-03-10 13:32:16 -0800

    Initial Commit

---
+++ AcronymLookup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AcronymLookup
+//
+//  Created by Sunny Egbo on 3/10/17.
+//  Copyright © 2017 Sunny Egbo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
